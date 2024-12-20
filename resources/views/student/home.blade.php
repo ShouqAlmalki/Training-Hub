a290304592ddeafaed0e3400d865bdf4398efa9f
ShouqAlmalki <shoog123.abd@gmail.com>
2024-12-21 01:43:21 +0300

    init commit

---
+++ resources/views/student/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+@section('title', 'home') Home
+@section('content')
+
+<h1>Welcome to the Home Page</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+    //return view('home');
+});
+//->name('home');
+
+function hello ($name) {
+     echo 'hello' . $name;
+}
